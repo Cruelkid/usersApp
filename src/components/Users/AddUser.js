190efcf284fb7b9ef8421875815387276a74d032
Cruelkid <sonofammit@gmail.com>
2021-08-17 09:12:46 +0300

    refactored folder structure, added card component, applied css modules

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,18 +1,22 @@
 import React from "react";
+import Card from "../UI/Card";
+import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
-    const AddUserHandler = e => {
+    const AddUserHandler = (e) => {
         e.preventDefault();
-    }
+    };
 
     return (
-        <form onSubmit={AddUserHandler}>
-            <label htmlFor="username">Add user</label>
-            <input id="username" type="text"></input>
-            <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number"></input>
-            <button type='submit'>Add user</button>
-        </form>
+        <Card className={styles.input}>
+            <form onSubmit={AddUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text"></input>
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number"></input>
+                <button type="submit">Add user</button>
+            </form>
+        </Card>
     );
 };
 
